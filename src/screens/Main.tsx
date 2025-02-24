4868e7ecc52523dc1a089b82dca0ab0862015e78
Melannie Cambiaso <melannie.cambiaso@perficient.com>
2025-02-24 15:34:29 -0300

    fix(download): add missing file

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -35,7 +35,7 @@ export const Main = () => {
               at Perficient as Mobile Analyst
             </Typography>
 
-            <Button href={`${import.meta.env.BASE_URL}/docs/CV_Melannie_Cambiaso.pdf`} download="CV_Melannie_Cambiaso.pdf" variant="outlined" color="secondary">
+            <Button href={"src/assets/CV_Melannie_Cambiaso.pdf"} download="CV_Melannie_Cambiaso.pdf" variant="outlined" color="secondary">
               Download CV
             </Button>
           </Box>

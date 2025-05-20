d9010cc41adabf00f53d77364ba8d55b4ac083ba
Melannie Cambiaso <melannie.cambiaso@gmail.com>
2025-05-20 06:38:55 -0400

    fix(header): update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,34 +6,34 @@ export const Header = () => {
     }
 
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center text-xs md:text-xl">
             <div className="flex bg-midnight rounded-full gap-4 p-2">
                 <button
-                className="font-monserrat bg-primary text-white rounded-3xl md:rounded-full p-1">
+                className="font-monserrat bg-primary text-white rounded-3xl md:rounded-full p-2">
                     Home
                 </button>
 
                 <button 
                     onClick={() => scrollTo("aboutme")} 
-                    className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-1">
+                    className="cursor-pointer font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-2">
                     About Me
                 </button>
 
                 <button 
                     onClick={() => scrollTo("projects")} 
-                    className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-1">
+                    className="cursor-pointer font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-2">
                     Projects
                 </button>
 
                 <button 
                     onClick={() => scrollTo("toolkit")} 
-                    className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-1">
+                    className="cursor-pointer font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-2">
                     Toolkit
                 </button>
 
                 <button 
                     onClick={() => scrollTo("contact")} 
-                    className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-1">
+                    className="cursor-pointer font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-2">
                     Contact
                 </button>
             </div>

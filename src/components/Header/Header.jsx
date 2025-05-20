11711e2971817c2e8c471ad458feba51cfa094c1
Melannie Cambiaso <melannie.cambiaso@gmail.com>
2025-05-20 06:15:29 -0400

    fix(header): improve header responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 
 export const Header = () => {
+
 
     const scrollTo = (page) => {
         document.getElementById(page)?.scrollIntoView({ behavior: 'smooth' });
@@ -7,24 +9,33 @@ export const Header = () => {
 
     return (
         <div className="flex justify-center">
-            <div className="flex bg-midnight rounded-full gap-4 p-5">
-                <button className="font-monserrat bg-primary text-white rounded-3xl md:rounded-full p-2">
+            <div className="flex bg-midnight rounded-full gap-4 p-2">
+                <button
+                className="font-monserrat bg-primary text-white rounded-3xl md:rounded-full p-1">
                     Home
                 </button>
 
-                <button onClick={() => scrollTo("aboutme")} className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-2">
+                <button 
+                    onClick={() => scrollTo("aboutme")} 
+                    className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-1">
                     About Me
                 </button>
 
-                <button onClick={() => scrollTo("projects")} className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-2">
+                <button 
+                    onClick={() => scrollTo("projects")} 
+                    className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-1">
                     Projects
                 </button>
 
-                <button onClick={() => scrollTo("toolkit")} className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-2">
+                <button 
+                    onClick={() => scrollTo("toolkit")} 
+                    className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-1">
                     Toolkit
                 </button>
 
-                <button onClick={() => scrollTo("contact")} className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-2">
+                <button 
+                    onClick={() => scrollTo("contact")} 
+                    className="font-monserrat hover:bg-secondary text-white rounded-3xl md:rounded-full p-1">
                     Contact
                 </button>
             </div>
